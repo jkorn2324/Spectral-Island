3a07ec83924b601488d8b207b8c56da2590ec123
ffsjkorn <jkorn2324swagg@gmail.com>
2021-01-29 23:07:19 -0800

    Other changes after merge

--- Assets/Scripts/Interactables/InteractableObjectSet.cs
+++ Assets/Scripts/Interactables/InteractableObjectSet.cs
@@ -33,6 +33,6 @@ public static class InteractableObjectSet
     /// <returns>An Interactable object.</returns>
     public static InteractableObject GetClosestObjectTo(Vector2 position)
     {
-
+        return null;
     }
 }
