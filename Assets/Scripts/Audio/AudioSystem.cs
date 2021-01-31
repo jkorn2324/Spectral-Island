3633d7b96a80d1a6c95ca325d9247023f9aa4742
ffsjkorn <jkorn2324swagg@gmail.com>
2021-01-30 16:04:44 -0800

    Basis for audio transitions implemented

---
+++ Assets/Scripts/Audio/AudioSystem.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct AudioTransitionData
+{
+    [SerializeField]
+    public float transitionOffset;
+    [SerializeField]
+    public string trackChannelFrom;
+    [SerializeField]
+    public string trackChannelTo;
+    [SerializeField, Range(0.0f, 20f)]
+    public float transitionTime;
+}
+
+/// <summary>
+/// The hacky audio system.
+/// </summary>
+[RequireComponent(typeof(Animator))]
+public class AudioSystem : MonoBehaviour
+{
+    [SerializeField]
+    private AudioTracksData trackData;
+    private AudioTracksSet _tracks;
+
+    [SerializeField]
+    private AudioTransitionData transitionData;
+
+    /// <summary>
+    /// Called when the audio system is called.
+    /// </summary>
+    private void Start()
+    {
+        this._tracks = new AudioTracksSet(trackData, this);
+        this._tracks.Play();
+
+        StartCoroutine(this.Transition(this.transitionData.transitionOffset));
+    }
+
+    private IEnumerator Transition(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        this._tracks.BeginTransition(
+            transitionData.trackChannelFrom, transitionData.trackChannelTo, transitionData.transitionTime);
+    }
+
+    /// <summary>
+    /// Updates the audio system.
+    /// </summary>
+    private void Update()
+    {
+        this._tracks.Update();
+    }
+}
---
+++ Assets/Scripts/Audio/AudioTransition.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Audio transition class.
+/// </summary>
+public class AudioTransition
+{
+    private AudioTrack _a, _b;
+
+    private float _maxTransitionTime;
+    private float _currentTransitionTime;
+
+    private float _endBVolume;
+    private float _startAVolume;
+    
+    private float TransitionPercentage
+        => this._currentTransitionTime / this._maxTransitionTime;
+
+    public bool IsFinished
+        => this.TransitionPercentage >= 1.0f;
+    
+    public AudioTransition(AudioTrack a, AudioTrack b, float transitionTime)
+    {
+        this._a = a;
+        this._b = b;
+        
+        this._endBVolume = b.ActiveVolume;
+        this._startAVolume = a.Volume;
+
+        this._currentTransitionTime = 0.0f;
+        this._maxTransitionTime = transitionTime;
+    }
+
+    /// <summary>
+    /// Updates the audio transition.
+    /// </summary>
+    /// <param name="deltaTime">The deltaTime.</param>
+    public void Update(float deltaTime)
+    {
+        this._currentTransitionTime += deltaTime;
+
+        float currentAVolume = Mathf.Lerp(
+            this._startAVolume, 0.0f, this._currentTransitionTime);
+        float currentBVolume = Mathf.Lerp(
+            0.0f, this._endBVolume, this._currentTransitionTime);
+        
+        this._a.Volume = currentAVolume;
+        this._b.Volume = currentBVolume;
+    }
+}
